90524977f32393a23d55516f16ebee1fc35af0dc
Juan5987 <juan.machado.5987@gmail.com>
2022-03-04 15:40:38 +0100

    added draw context hook

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,24 @@
+import React from 'react';
 import Header from 'components/Header';
 import Draw from 'components/Draw';
 import 'styles/app.scss';
 
+const draw = {
+  numbers: [7, 9, 16, 18, 22],
+  stars: [3, 9],
+}
+
+export const drawContext = React.createContext(draw);
+
 const App = () => {
+
   return (
-    <div className="app">
-      <Header/>
-      <Draw />
-    </div>
+    <drawContext.Provider value={draw}>
+      <div className="app">
+        <Header />
+        <Draw />
+      </div>
+    </drawContext.Provider>
   );
 }
 
--- src/components/Draw/index.js
+++ src/components/Draw/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { drawContext } from 'components/App';
 import 'styles/draw.scss';
 
 const Draw = () => {
+    const draw = useContext(drawContext);
+
     return (
         <section className="draw">
             <h2 className="draw__title">Tirage</h2>
             <div className="draw__container">
                 <div className="draw__container__result">  
                 <div className="draw__container__result__numbers">
-                    <span className="draw__container__result__numbers__number">7</span>
-                    <span className="draw__container__result__numbers__number">9</span>
-                    <span className="draw__container__result__numbers__number">16</span>
-                    <span className="draw__container__result__numbers__number">18</span>
-                    <span className="draw__container__result__numbers__number">22</span>
+                    {
+                        draw.numbers.map(number => <span key={number} className="draw__container__result__numbers__number">{number}</span>)
+                    }
                 </div>
                 <div className="draw__container__result__stars">
                     <span className="draw__container__result__stars__star">3</span>
